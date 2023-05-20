34ed223584f60f25d93c64cf21ba2ce8aee4a280
Leo Vargas <leo6vargas@gmail.com>
2023-05-20 17:49:39 -0600

    Tarea 1

---
+++ Tarea1.py
@@ -0,0 +1,19 @@
+# Solicitar los datos de los arreglos
+arreglo1 = []
+arreglo2 = []
+
+for i in range(3):
+    num = int(input("Ingrese un número para el arreglo 1: "))
+    
+
+for i in range(3):
+    num = int(input("Ingrese un número para el arreglo 2: "))
+    
+
+# Unir los dos arreglos en uno solo
+arreglo_completo = arreglo1 + arreglo2
+
+# Ordenar los números
+arreglo_completo.sort()
+
+print("Resultado:", arreglo_completo)
